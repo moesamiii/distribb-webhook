a0869ec8b8b65620367c6481cc75498bf7fdd94e
mohammed sami <110554475+moesamiii@users.noreply.github.com>
2026-05-03 14:53:20 +0300

    Update articles.js

--- api/articles.js
+++ api/articles.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // Handle CORS preflight
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -10,15 +9,14 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      "https://ivjarnsrwjmnzltsqhyp.supabase.co/rest/v1/articles?select=*&order=created_at.desc",
+      "https://bqbstpmltgccrhmtjufk.supabase.co/rest/v1/articles?select=*&order=created_at.desc",
       {
         headers: {
-          apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2amFybnNyd2ptbnpsdHNocXlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3OTI2MDksImV4cCI6MjA5MzM2ODYwOX0.s7_LWNT6X6xOWCowIDTz0rX-i7Lpp1UHRQHgl5dnI_U",
-          Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2amFybnNyd2ptbnpsdHNocXlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3OTI2MDksImV4cCI6MjA5MzM2ODYwOX0.s7_LWNT6X6xOWCowIDTz0rX-i7Lpp1UHRQHgl5dnI_U",
+          apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxYnN0cG1sdGdjY3JobXRqdWZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3OTcyMTYsImV4cCI6MjA5MzM3MzIxNn0.qpbVtz4ssj1jaFI2cCzssge7jGSOFJb9LG2L6bmW47k",
+          Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxYnN0cG1sdGdjY3JobXRqdWZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3OTcyMTYsImV4cCI6MjA5MzM3MzIxNn0.qpbVtz4ssj1jaFI2cCzssge7jGSOFJb9LG2L6bmW47k",
         },
       }
     );
-
     const data = await response.json();
     return res.status(200).json(data);
   } catch (error) {
